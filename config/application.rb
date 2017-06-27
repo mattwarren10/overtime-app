35f9be1a11ac8dec09e67307f434bbbe5b178a6b
Matt Warren <mwarren@harding.edu>
2017-06-27 10:57:17 -0400

    create sms stub for tests

--- config/application.rb
+++ config/application.rb
@@ -5,8 +5,7 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 
 module Overtime
-  class Application < Rails::Application
-  	config.active_record.raise_in_transactional_callbacks = true
+  class Application < Rails::Application  	
   	config.autoload_paths << Rails.root.join("lib")
   end
 end
