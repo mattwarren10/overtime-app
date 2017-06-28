d97559a62d0507d455c43e28de3b7607db7782e9
Matt Warren <mwarren@harding.edu>
2017-06-28 10:58:58 -0400

    block non admin users from accessing index page for audits

--- spec/features/audit_log_spec.rb
+++ spec/features/audit_log_spec.rb
@@ -16,7 +16,12 @@ describe 'audit log feature' do
 			visit audit_logs_path
 			expect(page).to have_content(/BIRD, LARRY/)
 		end
-		xit 'cannot be accessed by non admin users' do
+		it 'cannot be accessed by non admin users' do
+			logout(:user)
+			user = FactoryGirl.create(:user)
+			login_as(user, :scope => :user)
+			visit audit_logs_path
+			expect(current_path).to eq(root_path)
 		end
 	end
 end
