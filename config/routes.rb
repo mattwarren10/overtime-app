30ff463b1b5e3e0ca2ee6081731125cb087c5794
Matt Warren <mwarren@harding.edu>
2017-06-29 13:55:05 -0400

    update configs and initially generated files

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,3 @@
-
 Rails.application.routes.draw do
-
-  resources :audit_logs, except: [:new, :edit, :destroy] do
-    member do
-      get :confirm
-    end
-  end
-  namespace :admin do
-    resources :users
-    resources :posts
-    resources :admin_users
-
-    root to: "users#index"
-  end
-
-  resources :posts do
-    member do
-      get :approve
-    end
-  end
-  devise_for :users, skip: [:registrations]
-	root :to => 'static#homepage'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -25,18 +25,18 @@ ActiveRecord::Schema.define(version: 20170627203614) do
     t.index ["user_id"], name: "index_audit_logs_on_user_id", using: :btree
   end
 
-  create_table "posts", id: :bigserial, force: :cascade do |t|
+  create_table "posts", force: :cascade do |t|
     t.date     "date"
     t.text     "rationale"
     t.datetime "created_at",                       null: false
     t.datetime "updated_at",                       null: false
-    t.bigint   "user_id"
+    t.integer  "user_id"
     t.integer  "status",           default: 0
     t.decimal  "overtime_request", default: "0.0"
     t.index ["user_id"], name: "index_posts_on_user_id", using: :btree
   end
 
-  create_table "users", id: :bigserial, force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
